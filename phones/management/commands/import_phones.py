8f5248d335cd4a4f9116bc8610b36c4eb5f36277
Garfild13-06 <Garfild13-06@yandex.com>
2023-09-19 15:47:21 +0300

    19.09.2023 15:47

--- phones/management/commands/import_phones.py
+++ phones/management/commands/import_phones.py
@@ -1,8 +1,9 @@
 import csv
 
 from django.core.management.base import BaseCommand
-from phones.models import Phone
 
+from phones.models import Phone
+from slugify import slugify
 
 class Command(BaseCommand):
     def add_arguments(self, parser):
@@ -13,5 +14,12 @@ class Command(BaseCommand):
             phones = list(csv.DictReader(file, delimiter=';'))
         for phone in phones:
             # TODO: Добавьте сохранение модели
-            print(phone)
-            pass
+            Phone.objects.create(
+                id=phone["id"],
+                name=phone["name"],
+                image=phone["image"],
+                price=phone["price"],
+                release_date=phone["release_date"],
+                lte_exists=phone["lte_exists"],
+                slug=slugify(phone["name"])
+            )
